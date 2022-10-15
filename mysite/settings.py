e2f921abf849358be2c60b9783bc0401803cc3ca
woohoohooh@gmail.com <woohoohooh@whatever.com>
2022-10-15 14:55:15 +0700

    2

--- mysite/settings.py
+++ mysite/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-d_z+jd)pqojp(%9h0&i*2x6=5)m5cdxrs6$&hybffyi1k80qee'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
